78c6a994a2c9b2e79d7da007ff87ce1e1920c874
Elaine Leiyoung <92842749+elaineleiyoung@users.noreply.github.com>
2022-06-05 00:44:31 -1000

    added education page and jinja template

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,29 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+ 
+@app.route('/education')
+def education():
+    user_info = {
+        'hobbies':['Blogging','Reading Books','Playing chess'],
+        'education':{
+            'XX College',
+            'XX University'
+        }
+    }
+    return render_template('education.html', user = user_info)
