924748e3b04626d4cddced11eb797a1ff1619afb
Elaine Leiyoung <92842749+elaineleiyoung@users.noreply.github.com>
2022-06-06 01:32:03 -0700

    added work experience page
    uses jinja template, includes job name and details

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,37 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+STAR_USER_INFO = {
+    'hobbies':['Blogging','Reading Books','Playing chess'],
+    'experiences':{
+        'Job 1': ['Lorem ipsum dolor sit amet, consectetur adipiscing elit,' 
+                + 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ', 
+                'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' 
+                + ' ut aliquip ex ea commodo consequat.'],
+        'Job 2': ['did this', 'did that']
+    },
+    'education':{
+        'XX College': ['logo.jpg'],
+        'XX University': ['smallcats.png']
+    },
+    'images':[
+        'logo.jpg',
+        'smallcats.png'
+    ]
+}
+
+ELAINE_USER_INFO = {
+    'hobbies':['Blogging','Reading Books','Playing chess'],
+    'experiences': ['lorem ipsum', 'xxx', 'more description about work experiences'],
+    'education':{
+        'XX College': ['logo.jpg'],
+        'XX University': ['smallcats.png']
+    },
+    'images':[
+        'logo.jpg',
+        'smallcats.png'
+    ]
+}
 
 @app.route('/')
 def index():
