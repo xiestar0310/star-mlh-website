2cef9859cbf1bdc8107dc5ec71f4a97583e5d862
Star Xie <59263550+xiestar0310@users.noreply.github.com>
2022-07-03 18:21:31 -0400

    Create redeploy-site.sh

---
+++ redeploy-site.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ./project-code-monkeys
+
+git fetch && git reset origin/main --hard
+
+python -m venv python3-virtualenv
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+
+systemctl start myportfolio
+systemctl enable myportfolio
+systemctl daemon-reload
+systemctl restart myportfolio
+systemctl status myportfolio
